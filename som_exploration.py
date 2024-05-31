be97a49eaee9e580c0ef2333b56fd4602367015a
RATED-R-SUNDRAM <ss6437p@gmail.com>
2024-05-31 22:49:36 +0530

    ITIALIZED SOM CLASS

--- som_exploration.py
+++ som_exploration.py
@@ -7,5 +7,13 @@ import torch
 import torch.nn as nn
 
 #%%  
+class SOM(nn.Module):
+    def __init__(self, input_dim, hidden_dim):
+        super(SOM, self).__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.weights = nn.Parameter(torch.randn(hidden_dim, input_dim))
 
+    def forward(self, x):
+        return torch.matmul(self.weights, x)
 # experiment with scratch version of som
